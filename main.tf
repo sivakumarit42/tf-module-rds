c0b5fd46b107aeeec8fea90ef75f799c7e3ecfbc
Sivakumar S <sivakumarit42@gmail.com>
2023-03-28 10:21:56 +0530

    changes

--- main.tf
+++ main.tf
@@ -9,6 +9,7 @@ resource "aws_rds_cluster" "main" {
   preferred_backup_window = var.preferred_backup_window
   db_subnet_group_name    = aws_db_subnet_group.main.name
   skip_final_snapshot     = true
+  vpc_security_group_ids  = [aws_security_group.main.id]
 
   tags = merge(
     var.tags,
@@ -16,6 +17,34 @@ resource "aws_rds_cluster" "main" {
   )
 
 }
+
+resource "aws_security_group" "main" {
+  name        = "rds-${var.env}"
+  description = "rds-${var.env}"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description = "RDS"
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = var.allow_subnets
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = merge(
+    var.tags,
+    { Name = "rds-${var.env}" }
+  )
+}
+
 
 resource "aws_rds_cluster_instance" "main" {
   count              = var.no_of_instances
@@ -35,4 +64,10 @@ resource "aws_db_subnet_group" "main" {
     var.tags,
     { Name = "${var.env}-subnet-group" }
   )
+}
+
+resource "aws_ssm_parameter" "rds_endpoint" {
+  name  = "${var.env}.rds.endpoint"
+  type  = "String"
+  value = aws_rds_cluster.main.endpoint
 }
--- vars.tf
+++ vars.tf
@@ -11,3 +11,6 @@ variable "engine" {}
 variable "engine_version" {}
 variable "no_of_instances" {}
 variable "instance_class" {}
+variable "vpc_id" {}
+variable "allow_subnets" {}
+
